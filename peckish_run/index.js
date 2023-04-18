3bfdae5993b4fb17e9e7f5b1a999889b0e4d5ca5
amy null <null@amy.gg>
2023-04-18 16:07:25 -0400

    🐛 fix: Flags

--- peckish_run/index.js
+++ peckish_run/index.js
@@ -1,5 +1,6 @@
 import * as exec from "@actions/exec"
+import * as core from "@actions/core"
 
 export default async () => {
-  await exec.exec("peckish")
+  await exec.exec("peckish", ["-c", core.getInput("config")])
 }
